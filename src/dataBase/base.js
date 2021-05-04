47edcaed19cd13a8ad2926f910850c80ed6dafff
Mateus Silva Freitas <mateus.freitas7589@gmail.com>
2021-05-04 00:49:07 -0300

    Update base.js

--- src/dataBase/base.js
+++ src/dataBase/base.js
@@ -2,7 +2,7 @@ var AWS = require("aws-sdk");
 let awsConfig = {
     "region": "us-east-1",
     "endpoint": "http://dynamodb.us-east-1.amazonaws.com",
-    "accessKeyId": "AKIAYFLYSLWDT7KQBSNM", "secretAccessKey": "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"
+    "accessKeyId": "ID ACESS", "secretAccessKey": "ID SECRET"
 };
 AWS.config.update(awsConfig);
 
